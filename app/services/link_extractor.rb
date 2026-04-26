475777dbabd63ec7cc28a2e4f8cd50178781ff45
Mike Frizzell <mike.frizzell@gmail.com>
2026-04-26 09:06:27 -0500

    pull out links, internal and external

---
+++ app/services/link_extractor.rb
@@ -0,0 +1,54 @@
+require "nokogiri"
+require "uri"
+
+class LinkExtractor
+  Result = Struct.new(:internal, :external, keyword_init: true)
+
+  def self.call(html, base_url:)
+    new(html, base_url).call
+  end
+
+  def initialize(html, base_url)
+    @html = html.to_s
+    @base = URI.parse(base_url.to_s)
+  end
+
+  def call
+    return Result.new(internal: [], external: []) if @html.strip.empty?
+
+    doc = Nokogiri::HTML(@html)
+    internal = []
+    external = []
+
+    doc.css("a[href]").each do |a|
+      href = a["href"].to_s.strip
+      next if href.empty? || href.start_with?("#")
+
+      url = absolutize(href)
+      next unless url
+      next unless %w[http https].include?(url.scheme)
+
+      url.fragment = nil
+
+      if same_host?(url)
+        internal << url.to_s
+      else
+        external << url.to_s
+      end
+    end
+
+    Result.new(internal: internal.uniq, external: external.uniq)
+  end
+
+  private
+
+  def absolutize(href)
+    URI.join(@base, href)
+  rescue URI::InvalidURIError, ArgumentError
+    nil
+  end
+
+  def same_host?(url)
+    url.host&.downcase == @base.host&.downcase
+  end
+end
